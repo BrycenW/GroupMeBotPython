60b7e2e0f13e029e08f3f357f8cb44f2e348b2db
brycen <scoutingrulz@yahoo.com>
2015-09-21 04:31:39 -0400

    Fixed Gunicorn

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 #Brycen Wershing
-
-import os
 from flask import Flask
+import requests
 
-app = Flask(_name_)
-
+app = Flask(__name__)
 @app.route('/')
 def hello():
-		return 'hello'
+	return 'hello'
+
+if __name__ == '__main__':
+	app.run()
+
+payload = {"text":"hello", "bot_id":"7e819111ff8f330b299db0679f"}
+r = requests.post("https://api.groupme.com/v3/bots/post", params=payload)
+
