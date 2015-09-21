4a0672e015950ed8d564da7aa5ec8d069537566e
brycen <scoutingrulz@yahoo.com>
2015-09-21 04:40:57 -0400

    changed port on flask

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #Brycen Wershing
 from flask import Flask
 import requests
+import os
 
 app = Flask(__name__)
 @app.route('/')
@@ -8,7 +9,8 @@ def hello():
 	return 'hello'
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get("PORT",5000))
+	app.run(host='0.0.0.0', port=port)
 
 payload = {"text":"hello", "bot_id":"7e819111ff8f330b299db0679f"}
 r = requests.post("https://api.groupme.com/v3/bots/post", params=payload)
