bd5cacc106ce63b5896c829c4113ff7e85aa0b9b
brycen <scoutingrulz@yahoo.com>
2015-09-22 09:37:28 -0400

    broke out in and out

--- inOut.py
+++ inOut.py
@@ -1,4 +1,3 @@
-#Brycen Wershing
 from flask import Flask
 import requests
 import os
@@ -6,7 +5,9 @@ import ast
 import time
 from inOut import *
 
-'''
+def test():
+	print "you good"
+
 def grab_message_number(dirty_string):
 	for x in range (0, len(dirty_string)-6):
 		if dirty_string[x:x+5] == "count":
@@ -46,7 +47,6 @@ def seek(seeking_num):
 def post(text):
 	payload = {"text":text, "bot_id":"7e819111ff8f330b299db0679f"}
 	requests.post("https://api.groupme.com/v3/bots/post", params=payload)
-'''
 
 def analyse_and_output(message):
 	text = message['text']
@@ -54,48 +54,3 @@ def analyse_and_output(message):
 	if text[0:4] == "echo":
 		post(text[4:])
 	return 0
-
-
-last_num = 0
-while(1):
-	message = pull_message(last_num)
-	print message['count']
-	if int(message['count']) > last_num:
-		print message
-		analyse_and_output(message)
-		last_num = int(message['count']) + 1
-	else:
-		test()
-		time.sleep(.1)
-		last_num = int(message['count'])
-
-
-def antiquated():
-	payload = {"limit":1, "token":"dbce80c042ef0133562d05f0d49317f6"}
-	r = requests.get("https://api.groupme.com/v3/groups/16326365/messages", params=payload)
-	temp = r.content
-	for x in range (0, len(temp)-6):
-		if temp[x:x+5] == "count":
-			for y in range (x+5, len(temp)-6):
-				if temp[y:y+8] == "messages":
-					mes_id = temp[x+7: y-2]
-					print mes_id
-	if last_num != int(mes_id):
-		for x in range (0, len(temp)-6):
-			if temp[x:x+4] == "null":
-				temp = temp[:x] + "\"\"" + temp[x+4:]
-			if temp[x:x+5] == "false":
-				temp = temp[:x] + "\"0\"" + temp[x+5:]
-		for x in range (0, len(temp)-6):
-			if temp[x:x+8] == "messages":
-				for y in range (x + 12, len(temp)-6):
-					if temp[y:y+4] == "meta":
-						temp = temp[x+11: y-4]
-		message = eval(temp)
-		print message['text']
-		
-		payload = {"text":message['text'], "bot_id":"7e819111ff8f330b299db0679f"}
-		r = requests.post("https://api.groupme.com/v3/bots/post", params=payload)
-		last_num = int(mes_id) + 1
-
-
