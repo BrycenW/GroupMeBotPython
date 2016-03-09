2d17b20ea449f95980d7e3447aededab8a1641b8
Brycen <wershingbrycen@gmail.com>
2016-03-09 00:16:17 -0500

    fixin some shit

--- automod.py
+++ automod.py
@@ -9,23 +9,17 @@ token = raw_input("Input token: ")
 bot_id = raw_input("Input bot_id: ")
 group_id = raw_input("Input group_id: ")
 
-def check_mess(text):
-	print text
+def is_kickable(text):
 	if "kick me" in text.lower():
-		print "true"
 		return True
-	print "false"
 	return False
 
 def moderate():
 	last_num = 0
 	while 1:
 		message = pull_message(last_num, token, group_id)
-		print message['count']
 		if int(message['count']) > last_num:
-			print message
-			print message['text']
-			if check_mess(message['text']):
+			if is_kickable(message['text']):
 				kick_member(group_id, message['user_id'], token, group_id)
 			last_num = message['count']
 		else:
@@ -38,10 +32,7 @@ def rerename(user_id, nickname):
 	last_num = 0
 	while 1:
 		message = pull_message(last_num, token, group_id)
-		print message['count']
 		if int(message['count']) > last_num:
-			print message
-			print message['user_id'] + message['name']
 			if str(message['user_id']) == str(user_id) and nickname != message['name']:
 				rename_member(bot_id, user_id, token, group_id, nickname)
 			last_num = message['count']
